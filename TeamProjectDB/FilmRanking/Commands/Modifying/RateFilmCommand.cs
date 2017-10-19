bca984da11cd35d57d8c362dec03aac8738d02e9
Atanas Terziev <djnasi4@gmail.com>
2017-10-19 15:39:19 +0300

    Rate update

--- TeamProjectDB/FilmRanking/Commands/Modifying/RateFilmCommand.cs
+++ TeamProjectDB/FilmRanking/Commands/Modifying/RateFilmCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FilmRanking.Data;
+using FilmRanking.GUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,26 @@ namespace FilmRanking.Commands.Modifying
 {
     class RateFilmCommand
     {
+        private readonly IFilmMakingContext context;
+        private readonly GraphicInterfaces interfaceGenerator;
+
+        public RateFilmCommand(IFilmMakingContext context, GraphicInterfaces interfaceGenerator)
+        {
+            this.context = context;
+            this.interfaceGenerator = interfaceGenerator;
+        }
+
+
+        public void RateFilm()
+        {
+            Console.WriteLine(interfaceGenerator.Title());
+            string filmTitle = Console.ReadLine();
+            Console.WriteLine(interfaceGenerator.Rate());
+            double rate = double.Parse(Console.ReadLine());
+            var rateMovie = this.context.Films.Single(x => x.Title == filmTitle);
+            rateMovie.Rate = rate;
+            this.context.SaveChanges();
+
+        }
     }
 }
